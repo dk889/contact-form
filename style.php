941ab824c84620d49daeaad1e590ed86bf3e6be2
dk889 <74870930+dk889@users.noreply.github.com>
2020-11-22 22:37:57 +0530

    Add files via upload

---
+++ style.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<style type="text/css">
+
+/*{ margin: 0; padding:0; font-faimly: 'Mulish',sans-serif; box-sizing: border-box;}
+.divider-text {
+    position: relative;
+    text-align: center;
+    margin-top: 15px;
+    margin-bottom: 15 px;
+}
+
+.divider-text spam{
+    padding: 7px;
+    font-size: 12px
+    position: relative;
+    z-index: 2;
+}
+
+.divider-text:after{
+    content: "";
+    postition: absolute;
+    width: 100%;
+    border-bottom: 1px solid #ddd;
+    top: 55%;
+    left: 0;
+    z-index: 1;
+}
+
+.btn-facebook{
+    background-color: #405D9D!important;
+    color: #fff!important;
+}
+
+.btn-email{
+    background-color: #ea4335!important;
+    color: #fff!important;
+}
+
+</style>*/
+
+
+/* Style inputs with type="text", select elements and textareas */
+input[type=text], select, textarea {
+  width: 100%; /* Full width */
+  padding: 12px; /* Some padding */ 
+  border: 1px solid #ccc; /* Gray border */
+  border-radius: 4px; /* Rounded borders */
+  box-sizing: border-box; /* Make sure that padding and width stays in place */
+  margin-top: 6px; /* Add a top margin */
+  margin-bottom: 16px; /* Bottom margin */
+  resize: vertical /* Allow the user to vertically resize the textarea (not horizontally) */
+}
+/* Style the submit button with a specific background color etc */
+input[type=submit] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+/* When moving the mouse over the submit button, add a darker green color */
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+/* Add a background color and some padding around the form */
+.container {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+</style>
+</head>
+<body>
+
+</body>
+</html>
